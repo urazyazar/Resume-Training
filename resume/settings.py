fb11f3b93027d3280c16c259b5f76817efd40b7b
Your Name <yazaruraz@mgail.com>
2023-12-26 10:23:45 +0300

    feat: enable continuous development in docker

--- resume/settings.py
+++ resume/settings.py
@@ -16,6 +16,7 @@ import environ
 env = environ.Env(DEBUG=(bool, False))
 environ.Env.read_env()
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
